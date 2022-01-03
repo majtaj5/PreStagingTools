66cd220fa18b46fb7ab8751b87d9e79c75f0dc69
Piotr Krol <krolpiotr225@gmail.com>
2022-01-03 20:49:38 +0100

    Add project files.

---
+++ PreStagingTools/Models/GCodeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreStagingTools.Models
+{
+    public class GCodeModel
+    {
+        public string [] ToolChanges { get; set; }
+
+    }
+}
---
+++ PreStagingTools/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] paths = Environment.GetCommandLineArgs();
+
+
+
+Console.WriteLine(paths[1]);
+string[] lines = System.IO.File.ReadAllLines(paths[1]);
+System.Console.WriteLine("Jebac pis = ");
+foreach (string line in lines)
+{
+    
+    Console.WriteLine("\t" + line);
+}
+
+
+Console.WriteLine("napisnij cos");
+System.Console.ReadKey();
